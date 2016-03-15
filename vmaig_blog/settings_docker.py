aae949579626ddc01f8e98a42d109bcfcace4208
billvsme <994171686@qq.com>
2016-03-15 18:19:21 +0800

    Fix pep8

--- vmaig_blog/settings_docker.py
+++ vmaig_blog/settings_docker.py
@@ -81,8 +81,6 @@ WSGI_APPLICATION = 'vmaig_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db_vmaig',
         'USER': 'vmaig',
