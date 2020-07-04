4031f346a24c95f09360677dda4e979a3627237d
bobo <1744381765@qq.com>
2020-07-05 00:19:31 +0800

    change passwd

--- vmaig_blog/settings/dev.py
+++ vmaig_blog/settings/dev.py
@@ -9,10 +9,21 @@ WSGI_APPLICATION = 'vmaig_blog.wsgi_dev.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'boboblog',
+        'USER': 'root',
+        'PASSWORD': 'qqdb123456@',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
